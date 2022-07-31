d3436799a1bdf262572623f637b8833121524453
essilfiequansah <bquansah007@gmail.com>
2022-07-31 18:07:42 +0000

    add script: practicing arithmetic with 6103 in bash

---
+++ getrichquick.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "What is your name? "
+
+read name
+
+sleep 1
+
+echo "How old are you? "
+
+read age
+
+sleep 1
+
+getrich=$(( ($RANDOM % 15 ) + $age ))
+
+echo "Hey $name, you will become a millionaire when you are $getrich years old"
